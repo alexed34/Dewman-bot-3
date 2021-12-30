35cbfaf528fc58a670d7f309dce89d4d7d7dc809
alexed34 <you@example.com>
2021-12-30 17:15:40 +0300

    исправил import vk_api

--- vk_bot.py
+++ vk_bot.py
@@ -1,15 +1,15 @@
 import os
-from vk_api.longpoll import VkLongPoll, VkEventType
 from dotenv import load_dotenv
 import random
-import vk_api as vk
+import vk_api
 from telegram_bot import detect_intent_texts
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 load_dotenv()
 
 
-def send_message(event, vk_api, text):
-    vk_api.messages.send(
+def send_message(event, vk_ap, text):
+    vk_ap.messages.send(
         user_id=event.user_id,
         message=text,
         random_id=random.randint(1, 1000)
@@ -18,14 +18,14 @@ def send_message(event, vk_api, text):
 
 def main():
     vk_token = os.getenv('VK_TOKEN')
-    vk_session = vk.VkApi(token=vk_token)
-    vk_api = vk_session.get_api()
+    vk_session = vk_api.VkApi(token=vk_token)
+    vk_ap = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             text = detect_intent_texts(event.text)
             if text != 'Я не понимаю о чём речь':
-                send_message(event, vk_api, text)
+                send_message(event, vk_ap, text)
 
 
 if __name__ == '__main__':
